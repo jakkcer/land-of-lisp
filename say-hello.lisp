3733e5dbcd75d5adc432b2095eb284023ec4adc2
jakkcer <jakkcer.fivework@gmail.com>
2023-12-19 09:35:36 +0900

    first commit

---
+++ say-hello.lisp
@@ -0,0 +1,11 @@
+(defun say-hello ()
+  (print "Please type your name:")
+  (let ((name (read)))
+    (print "Nice to meet you, ")
+    (print name)))
+
+(defun say-hello2 ()
+  (princ "Please type your name:")
+  (let ((name (read-line)))
+    (princ "Nice to meet you, ")
+    (princ name)))
